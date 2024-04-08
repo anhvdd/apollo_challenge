c5239f2e25d876221f7e20d5f6235ef672334497
anhvdd <long.vu@devexperts.com>
2024-04-08 14:20:48 +0700

    create project

---
+++ src/main/java/com/example/apollochallenge/ApolloChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.apollochallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apollochallenge/ApolloChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apollochallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
